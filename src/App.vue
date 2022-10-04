da292988c922fdb6d3c8b8460205cfc75d4ead87
hasanpolatt <thatsnotpublic@gmail.com>
2022-10-04 17:46:16 +0300

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="dropdown-wrapper">
+    <div class="selected-item">
+      <span>Select One</span>
+    </div>
+    <div class="dropdown-popover">
+      <input v-model="searchQuery" type="text" placeholder="Search">
+      <div class="options">
+        <ul>
+          <li>İstanbul</li>
+          <li>Ankara</li>
+          <li>İzmir</li>
+          <li>Bursa</li>
+          <li>Diyarbakır</li>
+        </ul>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
