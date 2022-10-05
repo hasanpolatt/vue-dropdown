5d037302a8984524a2b71e3a19e9f357af8a3dec
hasanpolatt <thatsnotpublic@gmail.com>
2022-10-05 18:27:46 +0300

    added dropdown click

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <section class="selected-box">
-    <div class="select-item">
+    <div @click="isVisible= !isVisible" class="select-item">
       <span>Select One</span>
+      <svg
+        class="dropdown-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path d="M13 7.828V20h-2V7.828l-5.364 5.364-1.414-1.414L12 4l7.778 7.778-1.414 1.414L13 7.828z"/>
+      </svg>
     </div>
-    <div class="dropdown">
-      <input v-model="searchQuery" type="text" placeholder="Search">
+    <div v-if="isVisible" class="dropdown">
+      <input v-model="search" type="text" placeholder="Search">
       <div class="options">
         <ul>
           <li>İstanbul</li>
@@ -12,6 +22,7 @@
           <li>İzmir</li>
           <li>Bursa</li>
           <li>Diyarbakır</li>
+          <li>Eskişehir</li>
         </ul>
       </div>
     </div>
@@ -20,7 +31,15 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      search: '',
+      selectedItem: null,
+      isVisible: false,
+    }
+  },
+};
 </script>
 
 <style>
@@ -45,5 +64,39 @@ export default {};
     max-width: 300px;
     border: 2px solid lightgray;
     border-radius: 5px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    padding: 10px;
+  }
+
+  input {
+    width: 90%;
+    border: 2px solid lightgray;
+    border-radius: 5px;
+    font-size: 16px;
+    padding-left: 25px;
+  }
+
+  .options {
+    width: 100%;
+  }
+
+  ul {
+    list-style: none;
+    text-align: left;
+    padding-left: 8px;
+    max-height: 180px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+
+  li {
+    width: 100%;
+    border-bottom: 1px solid lightgrey;
+    padding: 10px;
+    background-color: #f3f3f3;
+    cursor: pointer;
+    font-size: 16px;
   }
 </style>
