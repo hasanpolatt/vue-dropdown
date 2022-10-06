51b48bee07b645deb1a09baf6b14f8dd315b7ed5
hasanpolatt <thatsnotpublic@gmail.com>
2022-10-06 14:16:31 +0300

    refactor: file editing

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,18 @@
 <template>
-  <section class="selected-box">
-    <div @click="isVisible= !isVisible" class="select-item" >
-      <span v-if="selectedItem">{{ selectedItem.name }}</span>
-      <input class="search" v-model="search" v-else placeholder="Select One">
-      <svg
-        :class="isVisible ? 'dropdown-icon-2' : ''"
-        class="dropdown-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="17"
-      >
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path d="M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"/>
-      </svg>
-      <span class="message" v-if="isVisible && filter.length === 0">No results found</span>
-      <div v-if="isVisible">
-        <div class="options">
-          <ul>
-            <li @click="selectItem(user)" v-for="(user, index) in filter" :key="`user-${index}`">{{ user.name }}</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </section>
+  <DropDown/>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      search: '',
-      selectedItem: null,
-      isVisible: false,
-      userArray: [],
-    }
-  },
-  computed: {
-    filter() {
-      const query = this.search.toLowerCase();
+import DropDown from './components/DropDown.vue'
 
-      if (this.search === "") {
-        return this.userArray;
-      }
-      return this.userArray.filter((user) => {
-        return Object.values(user).some((word) => 
-          String(word).toLowerCase().includes(query));
-      });
-    }
-  },
-  mounted() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
-      this.userArray = json;
-    });
-  },
-  methods: {
-    selectItem(user) {
-      this.selectedItem = user;
-      this.isVisible = false;
-    }
+export default {
+  name: 'App',
+  components: {
+    DropDown
   }
-};
+}
 </script>
 
 <style>
-  .selected-box {
-    max-width: 350px;
-    margin: auto;
-  }
-
-  .select-item {
-    border: 2px solid lightgray;
-    border-radius: 5px;
-  }
-
-  .search {
-    max-width: 250px;
-    border: 10px solid transparent;
-  }
-
-  textarea:focus, input:focus{
-    outline: none;
-  }
-
-  .dropdown-icon {
-    transform: rotate(0deg);
-    transition: all 0.3s;
-  }
-
-  .dropdown-icon-2 {
-    transform: rotate(180deg);
-    transition: all 0.3s;
-  }
-
-  input {
-    width: 90%;
-    border: 2px solid lightgray;
-    border-radius: 5px;
-    font-size: 16px;
-    padding-left: 25px;
-  }
-
-  .options {
-    margin-top: 10px;
-    width: 100%;
-  }
-
-  ul {
-    list-style: none;
-    text-align: left;
-    padding-left: 8px;
-    max-height: 180px;
-    overflow-y: scroll;
-    overflow-x: hidden;
-  }
-
-  li {
-    width: 100%;
-    border-bottom: 1px solid lightgrey;
-    padding: 10px;
-    background-color: #f3f3f3;
-    cursor: pointer;
-    font-size: 16px;
-  }
-
-  li:hover {
-    background: #a5a4a4;
-    color: #fff;
-    font-weight: bolder;
-  }
-
-  .message {
-    margin-left: 35px;
-  }
+  
 </style>
--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,58 +1,138 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section class="selected-box">
+    <div @click="isVisible= !isVisible" class="select-item" >
+      <span v-if="selectedItem">{{ selectedItem.name }}</span>
+      <input class="search" v-model="search" v-else placeholder="Select One">
+      <svg
+        :class="isVisible ? 'dropdown-icon-2' : ''"
+        class="dropdown-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="17"
+      >
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path d="M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"/>
+      </svg>
+      <span class="message" v-if="isVisible && filter.length === 0">No results found</span>
+      <div v-if="isVisible">
+        <div class="options">
+          <ul>
+            <li @click="selectItem(user)" v-for="(user, index) in filter" :key="`user-${index}`">{{ user.name }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      search: '',
+      selectedItem: null,
+      isVisible: false,
+      userArray: [],
+    }
+  },
+  computed: {
+    filter() {
+      const query = this.search.toLowerCase();
+
+      if (this.search === "") {
+        return this.userArray;
+      }
+      return this.userArray.filter((user) => {
+        return Object.values(user).some((word) => 
+          String(word).toLowerCase().includes(query));
+      });
+    }
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      this.userArray = json;
+    });
+  },
+  methods: {
+    selectItem(user) {
+      this.selectedItem = user;
+      this.isVisible = false;
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .selected-box {
+    max-width: 350px;
+    margin: auto;
+  }
+
+  .select-item {
+    border: 2px solid lightgray;
+    border-radius: 5px;
+  }
+
+  .search {
+    max-width: 250px;
+    border: 10px solid transparent;
+  }
+
+  textarea:focus, input:focus{
+    outline: none;
+  }
+
+  .dropdown-icon {
+    transform: rotate(0deg);
+    transition: all 0.3s;
+  }
+
+  .dropdown-icon-2 {
+    transform: rotate(180deg);
+    transition: all 0.3s;
+  }
+
+  input {
+    width: 90%;
+    border: 2px solid lightgray;
+    border-radius: 5px;
+    font-size: 16px;
+    padding-left: 25px;
+  }
+
+  .options {
+    margin-top: 10px;
+    width: 100%;
+  }
+
+  ul {
+    list-style: none;
+    text-align: left;
+    padding-left: 8px;
+    max-height: 180px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+
+  li {
+    width: 100%;
+    border-bottom: 1px solid lightgrey;
+    padding: 10px;
+    background-color: #f3f3f3;
+    cursor: pointer;
+    font-size: 16px;
+  }
+
+  li:hover {
+    background: #a5a4a4;
+    color: #fff;
+    font-weight: bolder;
+  }
+
+  .message {
+    margin-left: 35px;
+  }
 </style>
